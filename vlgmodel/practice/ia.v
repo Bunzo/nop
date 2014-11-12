ba7f00cb3b552922b365bd099000b1d6531064e3
bunzok <bunzok@iudicium.local>
2014-11-12 09:51:42 +0900

    Directory arrangement and practice envionments
    For easy to use.
    Verilog practice envionments.

---
+++ vlgmodel/practice/ia.v
@@ -0,0 +1,22 @@
+module ia (/*AUTOARG*/
+  // Outputs
+  addr,
+  // Inputs
+  clk, rst
+  );
+  parameter aw = 8;
+  
+  input                 clk, rst;
+  output [aw-1:0]       addr;
+  reg [aw-1:0]          pc;
+
+  always @(posedge clk or negedge rst) begin
+    if ( ! rst) begin
+      // Initialize pc[aw-1:0]
+    end else begin
+      // 
+      // pc ++
+    end
+  end
+
+endmodule // ia
