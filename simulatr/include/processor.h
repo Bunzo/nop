eed0da825d2d0bd7608333d98dc884188b997b53
bunzok <bunzok@iudicium.local>
2014-11-17 02:39:34 +0900

    Creating simulatr
    Under construction.

--- simulatr/include/processor.h
+++ simulatr/include/processor.h
@@ -4,12 +4,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #define Uint unsigned int
 #define Uchar unsigned char
 
 #define PENUM 1
 #define GRNUM  8
+#define PE(x) chip.p[x]
 
 #include "clk.h"
 #include "ia.h"
@@ -17,22 +19,23 @@
 #include "mmem.h"
 
 typedef struct {
-  Uint reg;
+	Uint reg;
 } reg_s;
 
 typedef struct {
-  ia_s plia;
-  if_s plif;
-  
-  reg_s gpr[GRNUM];
+	ia_s plia;
+	if_s plif;
+
+	reg_s gpr[GRNUM];
+	mmem_s imem;
+	mmem_s dmem;
 } proc_s;
 
 struct {
 	clk_s clk;
 	proc_s p[PENUM];
-  mmem_s imem;
-  mmem_s dmem;
+	mmem_s l2cache;
 } chip;
 
-
+void onintr();
 #endif
--- simulatr/src/processor.c
+++ simulatr/src/processor.c
@@ -4,5 +4,36 @@ int main(int argc, char **argv)
 {
 	clk_rst();
 
+	mmem_alloc(0, 100);
+	mmem_write(&PE(0).imem, 0, 0xdeadbeef);
+	printf("%x \n", mmem_read(&PE(0).imem, 0));
+	printf("%p \n", mmem_base(&PE(0).imem));
+	printf("%p \n", mmem_base(&PE(0).imem)+0x4);
+
+	onintr();
+
+	while (1) {
+		if (clk_stat() == POSEDGE_CLK){
+			pc_inc(0);
+		}
+		if (pc_pc(0) > 10) {
+			break;
+		}
+		clk_clk();
+		clk_showattr();
+	}
+
 	return 0;
+}
+
+void onintr()
+{
+	int i;
+	for (i=0; i<PENUM; i++) {
+		free(PE(i).imem.mem);
+		free(PE(i).dmem.mem);
+	}
+	free(chip.l2cache.mem);
+
+	exit(0);
 }
