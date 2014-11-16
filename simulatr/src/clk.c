12191e0ea4b3f30dcc747795b3d9a915dd51aa23
bunzok <bunzok@iudicium.local>
2014-11-17 02:39:34 +0900

    Creating simulatr
    Under construction.

--- simulatr/src/clk.c
+++ simulatr/src/clk.c
@@ -4,19 +4,37 @@ void clk_clk(void)
 {
 	chip.clk.prev_clk = chip.clk.clk;
 	chip.clk.clk++;
-	chip.clk.counter++;
+	if (clk_stat() == POSEDGE_CLK) {
+		chip.clk.counter++;
+	}
 }
 
 void clk_showattr()
 {
-	printf("clk: %d, prev_clk: %d \n", chip.clk.clk, chip.clk.prev_clk);
+	printf("clk: %d, prev_clk: %d, cycle counter: %d \n", 
+			chip.clk.clk, chip.clk.prev_clk, chip.clk.counter);
 }
 
 void clk_rst(void)
 {
 	printf("clk_rst\n");
 	clk_showattr();
-	chip.clk.clk = 1;
-	chip.clk.prev_clk = 0;
+	chip.clk.clk = 0;
+	chip.clk.prev_clk = 1;
 	chip.clk.counter = 0;
+}
+
+Uint clk_stat()
+{
+	if (chip.clk.clk == 0x1 && chip.clk.prev_clk == 0x0) {
+		return POSEDGE_CLK;
+	}	else if (chip.clk.clk == 0x0 && chip.clk.prev_clk == 0x1) {
+		return NEGEDGE_CLK;
+	}	else if (chip.clk.clk == 0x1 && chip.clk.prev_clk == 0x1) {
+		return CLKSTOP_POS;
+	}	else if (chip.clk.clk == 0x0 && chip.clk.prev_clk == 0x0) {
+		return CLKSTOP_NEG;
+	}
+
+	return -1;
 }
