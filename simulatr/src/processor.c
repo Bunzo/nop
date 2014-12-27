f8aee3007fed23fd15de9384d5f1358ceeca1169
Bunzo-K <bunzo.lunch@gmail.com>
2014-12-28 04:02:51 +0900

    Condition register implemented.

--- simulatr/src/processor.c
+++ simulatr/src/processor.c
@@ -7,12 +7,10 @@ int main(int argc, char **argv)
 
 	while (1) {
 		if (clk_stat() == POSEDGE_CLK){
+			mmem_write(&PE(0).imem, pc_pc(0), 0xdeadbeef);
 			pc_inc(0);
 		}
-		if (pc_pc(0) > 1) {
-			break;
-		}
-		mmem_write(&PE(0).imem, pc_pc(0), 0xdeadbeef);
+
 		printf("D: %x \n", mmem_read(&PE(0).imem, pc_pc(0)));
 		clk_clk();
 #ifdef DEBUG
