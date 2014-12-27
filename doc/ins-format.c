1ddfe4c4cdd1c149d2d3b32fbcf5a23a9ad5172a
Bunzo-K <bunzo.lunch@gmail.com>
2014-12-28 04:29:53 +0900

    instruction format decided.

--- doc/ins-format.c
+++ doc/ins-format.c
@@ -5,3 +5,14 @@ typedef struct {
 	unsigned int op2: 8;
 } ins;
 
+// Instructions
+// 0x00		add  op1, op2  : op1 = op1 + op2
+// 0x01		addi op1, #IMM : op1 = op1 + IMM (IMM : number)
+// 0x02		sub  op1, op2  : op1 = op1 - op2
+// 0x03		subi op1, #IMM : op1 = op1 - IMM (IMM : number)
+// 0x04		mul  op1, op2  : op1 = op1 * op2
+// 0x05		muli op1, #IMM : op1 = op1 * IMM (IMM : number)
+// 0x06		ld   op1, op2  : op1 = mem[op2]
+// 0x07		ldi  op1, #IMM : op1 = IMM[op2]
+// 0x08		st   op1, op2  : mem[ op1 ] = op2
+// 0x09		sti  op1, #IMM : mem[ op1 ] = IMM
